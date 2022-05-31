bb25f7c5ea42d5f2fb98dbbd4a25406b5308a605
dikshanain100 <dikshanain100@gmail.com>
2022-05-31 12:04:13 +0530

    offline service module imported in shared folder

--- Offline-Online-Sync-Client/src/app/shared/services/services.module.ts
+++ Offline-Online-Sync-Client/src/app/shared/services/services.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {InternalHttpService} from './internal-http.service';
+import { OfflineService } from './offline.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  providers:[InternalHttpService]
+  providers:[InternalHttpService, OfflineService]
 })
 
 export class ServicesModule { }
