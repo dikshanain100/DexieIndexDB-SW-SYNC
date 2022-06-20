441ba1abe5e3fde3fb393226af782e0404a0dc5e
dikshanain100 <dikshanain100@gmail.com>
2022-06-20 13:01:53 +0530

    login form basics done, need to work on validation

--- Offline-Online-Sync-Client/src/app/login/login.component.ts
+++ Offline-Online-Sync-Client/src/app/login/login.component.ts
@@ -1,18 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { LoginService } from './login.service';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss'],
+  changeDetection : ChangeDetectionStrategy.Default
 })
 export class LoginComponent implements OnInit {
 
+  customerForm: FormGroup;
+  submitted: boolean = false;
+
+
+
   constructor(
-    private _loginService: LoginService
-  ) { }
+    private _loginService: LoginService,
+    public formBuilder: FormBuilder,
+  ) {
+   
+   }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+
+  
+  public createForm() {
+    this.customerForm = this.formBuilder.group({
+      customer_email: ['', Validators.required],
+      customer_password: ['', Validators.required],
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get fetch() {
+    return this.customerForm.controls;
+  }
+
+  public submitForm() {
+    this.submitted = true;
+    var reqObj = {
+      custEmail: '',
+      custPassword : ''
+    };
+
+    if (
+      this.customerForm.controls.customer_email.valid && 
+      this.customerForm.controls.customer_password.valid
+    ) {
+      console.log('form valid ', this.customerForm);
+      reqObj.custEmail = this.customerForm.controls.customer_email.value;
+      reqObj.custPassword = this.customerForm.controls.customer_password.value;
+
+     // this.add(reqObj);
+    } else {
+      console.log('form invalid');
+      console.log('form ', this.customerForm);
+    }
+    console.log('this.submitted :: ', this.submitted);
+    this.submitted = false;
+  }
+
+  onCancel() {
+    console.log('Form Cancel clicked');
+    this.customerForm.reset();
+    this.customerForm.markAsPristine;
+    this.customerForm.markAsUntouched;
+
+    this.submitted = false;
   }
 
 }
