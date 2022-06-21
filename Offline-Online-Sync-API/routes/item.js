4f6c5b22a4e9dd96191b64ed51b3d56ae7e98bc9
dikshanain100 <dikshanain100@gmail.com>
2022-06-21 09:07:07 +0530

    basic node js code done for login and register

--- Offline-Online-Sync-API/routes/item.js
+++ Offline-Online-Sync-API/routes/item.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const TodoModel = require("../models/todo");
 const Mongoose = require("mongoose");
 const https = require('https');
+const UserModel = require("../models/user");
+const bcrypt = require("bcryptjs");  //so that passwords are not recognized or changed by anyone
+
+
 
 router.get("/todos", (req, res, next) => {
   //return all items
@@ -114,25 +118,67 @@ router.get("/entries", (req, res, next) => {
 
 
 
-router.post("/login", (req, res, next) => {
-  console.log('inside login :: ', req.body)
-  // TodoModel.insertMany(req.body, (err, docs) => {
-  //   if (err) {
-  //     res.status(400).json({
-  //       message: "The Todos were not saved",
-  //       errorMessage: err.message
-  //     })
-  //   } else {
-  //     res.status(200).json({
-  //       message: "Bulk document creation successful",
+router.post("/login",  (req, res, next) => {
+  const { custEmail, custPassword } = req.body;
 
-  //     })
-  //   }
-  // })
+  UserModel.findOne({email : custEmail}, (err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('user :: ', user)
+      if (!user) {
+        return res.redirect('/login');
+      }
+      else{
+       // const isMatch = await bcrypt.compare(custPassword, user.password)
+        const isMatch =  bcrypt.compare(custPassword, user.password)
+        if(!isMatch){
+          return res.redirect('/login')
+        }
+      
+        res.redirect('landing-page');
+      }
+
+      // res.status(200).json({
+      //   data: user
+      // });
+    }
+
+  })
+
+
+
 })
 
-router.post("/register", (req, res, next) => {
-  console.log('inside register :: ', req.body)
+router.post("/register", async (req, res, next) => {
+  const { custUsername, custEmail, custPassword } = req.body;
+
+  let user = await UserModel.findOne({ custEmail });
+  //check this
+  if (user) {
+    return res.redirect('/register');
+  }
+
+  const hashedPwd = await bcrypt.hash(custPassword, 12);
+  let userData = new UserModel({
+    username: custUsername,
+    email: custEmail,
+    password:hashedPwd
+  });
+
+  userData.save((err) => {
+    if (err) {
+      res.status(400).json({
+        message: "The user data was not saved",
+        errorMessage: err.message
+      })
+    } else {
+      res.status(201).json({
+        message: "User data was saved successfully"
+      })
+    }
+  })
+
 
 })
 
